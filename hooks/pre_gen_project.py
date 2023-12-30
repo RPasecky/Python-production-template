ad164f759f4f235e871341b2acbea8c86f6668e7
Ryan Pasecky <ryanpasecky2018@u.northwestern.edu>
2023-12-30 16:14:08 -0500

    Fixing import errors on pre-hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,13 +4,14 @@ import sys
 from cookiecutter.main import cookiecutter
 
 
+
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
 def find_conda_base_path():
     try:
         # Use 'where' on Windows, 'which' on Unix-like systems
         command = 'which' 
-        result = subprocess.check_output([command, 'conda']).decode().strip()
+        result = os.subprocess.check_output([command, 'conda']).decode().strip()
         print(result)
         # Split the output and get the first result
         conda_path = result.splitlines()[0]
@@ -18,7 +19,7 @@ def find_conda_base_path():
         # Extract the base path (two levels up from the actual 'conda' executable)
         base_path = os.path.dirname(os.path.dirname(conda_path))
         return base_path
-    except subprocess.CalledProcessError:
+    except os.subprocess.CalledProcessError:
         print("Conda is not installed or not in the PATH.")
         sys.exit(1)
 
