df27445601f7f0790e37ea7808d0166143806831
Ryan Pasecky <ryanpasecky2018@u.northwestern.edu>
2023-12-30 16:19:57 -0500

    Fixed subprocess error

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import re
 import sys
 from cookiecutter.main import cookiecutter
@@ -11,7 +12,7 @@ def find_conda_base_path():
     try:
         # Use 'where' on Windows, 'which' on Unix-like systems
         command = 'which' 
-        result = os.subprocess.check_output([command, 'conda']).decode().strip()
+        result = subprocess.check_output([command, 'conda']).decode().strip()
         print(result)
         # Split the output and get the first result
         conda_path = result.splitlines()[0]
@@ -19,7 +20,7 @@ def find_conda_base_path():
         # Extract the base path (two levels up from the actual 'conda' executable)
         base_path = os.path.dirname(os.path.dirname(conda_path))
         return base_path
-    except os.subprocess.CalledProcessError:
+    except subprocess.CalledProcessError:
         print("Conda is not installed or not in the PATH.")
         sys.exit(1)
 
